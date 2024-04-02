cee687c701696feea2ad56c4a87a00720ed2fcbe
BinaryBitBytes <117mwc117@gmail.com>
2024-04-01 22:36:56 -0400

    added notes

--- hello.c
+++ hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h> // searched for the STD_io library header; ends in .h extension
 
+//? code hello.c
+
+//? make hello
+
+//? ./hello
+
 int main(void)
 {
     printf("Hello, world");
 }
+
+// string answer = get_string("What is your name?");
